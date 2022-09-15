71deffe05102a9099f1e670723ffd2d3da23516e
lostsquirrel <lostsquirreli@hotmail.com>
2022-09-15 16:31:38 +0800

    add unittest basic

---
+++ test-demos/bin/test_demos.dart
@@ -0,0 +1,5 @@
+import 'package:test_demos/test_demos.dart' as test_demos;
+
+void main(List<String> arguments) {
+  print('Hello world: ${test_demos.calculate()}!');
+}
---
+++ test-demos/test/setup_teardown_test.dart
@@ -0,0 +1,20 @@
+import 'dart:io';
+
+import 'package:test/test.dart';
+
+void main() {
+  late HttpServer? server;
+  late Uri? url;
+  setUp(() async {
+    server = await HttpServer.bind('localhost', 0);
+    url = Uri.parse('http://${server.address.host}:${server.port}');
+  });
+
+  tearDown(() async {
+    await server?.close(force: true);
+    server = null;
+    url = null;
+  });
+
+  // ...
+}
