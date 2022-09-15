404cb79929dbac7357f0d3764eb660aca0bb234c
lostsquirrel <lostsquirreli@hotmail.com>
2022-09-15 16:34:14 +0800

    init project

---
+++ tour-demos/bin/tour_demos.dart
@@ -0,0 +1,5 @@
+import 'package:tour_demos/tour_demos.dart' as tour_demos;
+
+void main(List<String> arguments) {
+  print('Hello world: ${tour_demos.calculate()}!');
+}
---
+++ tour-demos/test/tour_demos_test.dart
@@ -0,0 +1,8 @@
+import 'package:tour_demos/tour_demos.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
