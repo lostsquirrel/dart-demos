295b26560a5f91279f3bef8a52f7ce5bd73061d4
lostsquirrel <lostsquirreli@hotmail.com>
2022-09-17 17:46:39 +0800

    add string test

---
+++ tour-demos/test/string_test.dart
@@ -0,0 +1,26 @@
+import 'package:test/expect.dart';
+import 'package:test/scaffolding.dart';
+
+void main() {
+  test("String in multi lines", () {
+    var s1 = 'String '
+        'concatenation'
+        ' works even over line breaks.';
+    var s2 = 'String ' + 'concatenation' + ' works even over line breaks.';
+    expect(s1, s2);
+  });
+
+  test("multi lines String", () {
+    var s1 = '''You can create 
+    multi-line strings like this one.
+    ''';
+    var s2 = 'You can create \n    multi-line strings like this one.\n    ';
+    print(s2);
+    expect(s1, s2);
+  });
+
+  test("raw string", () {
+    var s1 = r'raw string \n \t \r';
+    print(s1);
+  });
+}
